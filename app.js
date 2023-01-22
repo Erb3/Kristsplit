f2a544c24465bc3370592d794af360b3642fc89d
Erlend <49862976+Erb3@users.noreply.github.com>
2023-01-22 16:29:15 +0100

    Get MVP done

--- app.js
+++ app.js
@@ -1,1 +1,63 @@
-console.log("Hello world")
+import kristLib from 'krist';
+import fs from 'fs';
+const krist = new kristLib.KristApi();
+
+const configFile = await fs.promises.readFile('./config.json');
+const config = JSON.parse(configFile);
+
+const registeredSplits = {};
+
+config.forEach(async (val) => {
+  const [address] = await kristLib.calculateAddress(val.input, undefined, val.inputFormat);
+
+  registeredSplits[address] = {
+    privatekey: val.input,
+    privatekeyFormat: val.inputFormat,
+    split: val.output,
+    leftOvers: val.rest,
+  };
+});
+
+const ws = krist.createWsClient({
+  initialSubscriptions: ['transactions'],
+});
+
+ws.on('transaction', async ({ transaction }) => {
+  if (!registeredSplits[transaction.to]) {
+    return;
+  }
+
+  console.log('Splitting transaction: ', transaction);
+  const percent = transaction.value / 100;
+
+  const splits = registeredSplits[transaction.to];
+  let left = transaction.value;
+  splits.split.forEach((split) => {
+    const kristAmount = Math.floor(percent * split.percent);
+    left -= kristAmount;
+
+    if (kristAmount != 0) {
+      krist.makeTransaction(split.address, kristAmount, {
+        walletFormat: splits.privatekeyFormat,
+        privatekey: splits.privatekey,
+        metadata: 'Powered by KristSplit',
+      });
+    }
+  });
+
+  if (left != 0 && splits.leftOvers == 'REFUND') {
+    console.log('Refunding leftovers: ' + left);
+    krist.makeTransaction(transaction.from, left, {
+      walletFormat: splits.privatekeyFormat,
+      privatekey: splits.privatekey,
+      metadata: 'Powered by=KristSplit;donate=true;message=Here is the leftovers that could not be split!',
+    });
+  }
+});
+
+ws.on('ready', async () => {
+  const me = await ws.getMe();
+  console.log('Connected to WebSocket! I am: ', me);
+});
+
+ws.connect();
